bb430881a3e0b20cfacbada126fa2d1a7aa223e1
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2018-08-16 00:06:15 +0530

    dynamic urls, nav template

--- issue_tracking_system/settings.py
+++ issue_tracking_system/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # context processors
+                'app.context_processors.baseurl',
             ],
         },
     },
